b7fdf5e5c7174f340aae820fb2bcf00baa7c51c6
andrew <arenn@design.upenn.edu>
2020-07-14 19:57:07 -0400

    Half way?

--- R/proceed.R
+++ R/proceed.R
@@ -1,6 +1,106 @@
 source("R/help.R")
 source("R/package.R")
 
+## Background
+national_ages <-
+  bind_rows(read_xls("data/hackathon/aging.xls", skip = 5, sheet = 2) %>%
+              clean_names() %>%
+              filter(area == "England" & age_group != "All ages") %>%
+              separate(age_group, sep = "-", into = c("lower", "upper")) %>%
+              replace_na(list("upper" = 94)) %>%
+              mutate(upper = as.numeric(upper)) %>%
+              select(upper, x2016:x2041) %>%
+              gather(year, population, x2016:x2036) %>%
+              mutate(population = population * -1, group = "male"),
+            read_xls("data/hackathon/aging.xls", skip = 5, sheet = 3) %>%
+              clean_names() %>%
+              filter(area == "England" & age_group != "All ages") %>%
+              separate(age_group, sep = "-", into = c("lower", "upper")) %>%
+              replace_na(list("upper" = 94)) %>%
+              mutate(upper = as.numeric(upper)) %>%
+              select(upper, x2016:x2041) %>%
+              gather(year, population, x2016:x2036) %>%
+              mutate(group = "female"))
+
+##
+
+library(gganimate)
+
+##
+
+groups <- 
+  read_xls("data/hackathon/aging.xls", skip = 5, sheet = 2) %>%
+  clean_names() %>%
+  filter(area == "England" & age_group != "All ages") %>%
+  separate(age_group, sep = "-", into = c("lower", "upper"), remove = FALSE) %>%
+  replace_na(list("upper" = 94)) %>%
+  mutate(upper = as.numeric(upper)) %>%
+  select(age_group, lower, upper) %>%
+  group_by(age_group) %>%
+  slice(1)
+
+##
+
+national_aging <- 
+  ggplot(national_ages %>%
+           left_join(groups) %>%
+           mutate(year = str_replace_all(year, pattern = "x", replacement = ""))) +
+  geom_bar(aes(y = population, x = upper, fill = group), stat = 'identity',
+           show.legend = FALSE) +
+  geom_text(aes(x = upper, y = 0, label = age_group), colour = '#ffffff') +
+  scale_fill_manual(values = c(scico(palette = "grayC", 10)[3], scico(palette = "grayC", 10)[10])) +
+  scale_y_continuous(labels = abs, limits = max(national_ages$population, na.rm = TRUE) * c(-1, 1)) +
+  scale_x_continuous(labels = c("", "", ""), breaks = c(25, 50, 75), limits = c(-1, 100)) +
+  transition_states(year) +
+  ease_aes('linear') +
+  labs(title = "national projections {closest_state}",
+       subtitle = "PROJECTED POPULATION BY AGE",
+       x = "age bracket", y = "population") +
+  coord_flip() +
+  theme_pop()
+
+##
+
+anim_save(national_aging, filename = "aging.gif")
+
+##
+
+age_2016 <- 
+  ggplot(national_ages %>%
+           left_join(groups) %>%
+           mutate(year = str_replace_all(year, pattern = "x", replacement = "")) %>%
+           filter(year == "2016")) +
+  geom_bar(aes(y = population, x = upper, fill = group), stat = 'identity',
+           show.legend = FALSE) +
+  geom_text(aes(x = upper, y = 0, label = age_group), colour = '#ffffff') +
+  scale_fill_manual(values = c(scico(palette = "grayC", 10)[3], scico(palette = "grayC", 10)[10])) +
+  scale_y_continuous(labels = abs, limits = max(national_ages$population, na.rm = TRUE) * c(-1, 1)) +
+  scale_x_continuous(labels = c("", "", ""), breaks = c(25, 50, 75), limits = c(-1, 100)) +
+  labs(title = "national projections {closest_state}",
+       subtitle = "PROJECTED POPULATION BY AGE",
+       x = "age bracket", y = "population") +
+  coord_flip() +
+  theme_pop()
+
+age_2036 <- 
+  ggplot(national_ages %>%
+           left_join(groups) %>%
+           mutate(year = str_replace_all(year, pattern = "x", replacement = "")) %>%
+           filter(year == "2036")) +
+  geom_bar(aes(y = population, x = upper, fill = group), stat = 'identity',
+           show.legend = FALSE) +
+  geom_text(aes(x = upper, y = 0, label = age_group), colour = '#ffffff') +
+  scale_fill_manual(values = c(scico(palette = "grayC", 10)[3], scico(palette = "grayC", 10)[10])) +
+  scale_y_continuous(labels = abs, limits = max(national_ages$population, na.rm = TRUE) * c(-1, 1)) +
+  scale_x_continuous(labels = c("", "", ""), breaks = c(25, 50, 75), limits = c(-1, 100)) +
+  labs(title = "national projections {closest_state}",
+       subtitle = "PROJECTED POPULATION BY AGE",
+       x = "age bracket", y = "population") +
+  coord_flip() +
+  theme_pop()
+
+ggsave(age_2016, filename = "age2016.png", height = 5, width = 5, dpi = 300)
+ggsave(age_2036, filename = "age2036.png", height = 5, width = 5, dpi = 300)
 
 ## Data
 expectancy <- 
@@ -91,7 +191,69 @@ tmap_mode("view")
 tm_shape(authorities) +
   tm_polygons()
 
+## Data
+ahah <- read_csv("data/hackathon/ahahinputs.csv")
+
+area <-   
+  read_sf("data/hackathon/area.shp") %>%
+  st_drop_geometry() %>%
+  transmute(lsoa11 = lsoa01cd,
+            code = lad17cd) %>%
+  left_join(ahah) %>%
+  select(lsoa11:green900) %>%
+  group_by(code) %>%
+  summarise_if(is.numeric, mean, na.rm = TRUE) %>%
+  select(-seqnum)
+
+## Extra data
+rururb <- 
+  read_csv("data/hackathon/rururb.csv") %>%
+  clean_names() %>%
+  rename(code = lad11cd) %>%
+  select(code, total_rural_population_2011:total_population_2011, ruc11, broad_ruc11) %>%
+  rename(class = ruc11,
+         class_broad = broad_ruc11)
+
 ##
+
+deaths <- read_csv("data/hackathon/deaths.csv") %>%
+  clean_names() %>%
+  rename(code = lad11cd) %>%
+  select(code, deaths_num:agestand_mortality)
+
+##
+
+income <- 
+  read_xlsx("data/hackathon/income.xlsx", sheet = 2, skip = 2) %>%
+  clean_names() %>%
+  rename(code = lau1_code) %>%
+  select(code, region, x1998:x2016)
+
+names(income) <- str_replace_all(names(income), pattern = "x", replacement = "income_")
+
+## Regressing
+
+## Moransiing
+
+
+##
+
+box <- 
+  st_transform(background, 27700) %>% 
+  st_bbox() %>%
+  st_as_sfc() %>%
+  st_make_grid(cellsize = 50000) %>%
+  st_as_sf() %>%
+  rownames_to_column() %>% 
+  mutate(rowname = as.numeric(rowname))
+
+##
+
+plot(box)
+
+##
+
+
 
 
 
