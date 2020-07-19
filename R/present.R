f221f14ed3101c25b9c8c41b317b89ab4d671099
andrew <arenn@design.upenn.edu>
2020-07-19 09:47:05 -0400

    Better demo

--- R/present.R
+++ R/present.R
@@ -111,8 +111,18 @@ play <-
   expectancy %>%
   left_join(authorities) %>%
   filter(code != "E06000053") %>%
+  st_as_sf() 
+
+play <- 
+  play %>%
+  st_centroid() %>%
+  st_coordinates() %>%
+  as_tibble() %>%
+  bind_cols(play) %>%
+  arrange(Y) %>%
   rownames_to_column() %>%
   mutate(rowname = as.numeric(rowname)) %>%
+  select(-X, -Y) %>%
   st_as_sf() 
  
 
@@ -182,6 +192,7 @@ models <-
   geom_point(aes(colour = score), show.legend = FALSE) +
   geom_smooth(method = lm, fullrange = TRUE, colour = 'grey') +
   scale_colour_scico(palette = 'buda', direction = -1) +
+  scale_y_continuous(breaks = c(55, 60, 65, 70), limits = c(50, 75)) +
   xlab("English Index of Multiple Deprivation") +
   ylab("Healthy Life Expectancy") +
   transition_manual(id) +
@@ -190,6 +201,80 @@ models <-
 
 anim_save(models, filename = "models.gif", fps = 3)
 
+##
 
+frst_degree <- 
+  play %>% 
+  st_touches() %>% 
+  as_tibble() %>% 
+  clean_names()
 
+scnd_degree <- 
+  frst_degree %>%
+  rename(id = row_id,
+         row_id = col_id) %>%
+  left_join(frst_degree) %>%
+  transmute(id = id,
+            row_id = col_id) %>%
+  left_join(frst_degree) %>%
+  transmute(id = id,
+            row_id = col_id) %>%
+  left_join(frst_degree) %>%
+  transmute(id = id,
+            rowname = col_id) %>%
+  group_by(id) %>%
+  distinct(rowname, .keep_all = TRUE) %>%
+  ungroup() %>%
+  left_join(play) %>%
+  st_as_sf()
+
+scnd_degree %>% st_drop_geometry() %>% group_by(id) %>% summarise(n = n ())
+geom <- st_geometry(scnd_degree)
+
+final <- 
+  scnd_degree %>%
+  st_centroid() %>%
+  st_coordinates() %>%
+  as_tibble() %>%
+  bind_cols(scnd_degree) %>%
+  st_as_sf() %>%
+  st_drop_geometry() %>%
+  group_by(id) %>%
+  mutate(x = abs(X - mean(X)),
+         y = abs(Y - mean(Y))) %>%
+  mutate(score = (x + y) / 2) %>%
+  ungroup() %>% 
+  mutate(geometry = geom) %>%
+  st_as_sf()
+
+windows <- 
+  ggplot(final) +
+  geom_sf(data = background,
+          aes(), 
+          fill = 'grey70', colour = NA, size = 0) +
+  geom_sf(aes(fill = score), show.legend = FALSE) +
+  scale_fill_scico(palette = 'buda', direction = -1) +
+  transition_manual(id) +
+  ease_aes('linear') +
+  theme_map()
+
+anim_save(windows, filename = "gwr_larger.gif", fps = 3)
+
+names(final)
+
+models <- 
+  ggplot(final %>%
+           mutate(HLE = (healthy_life_expectancy_for_females_2009_2013_years + healthy_life_expectancy_for_males_2009_2013) / 2),
+         aes(x = index_of_multiple_deprivation_imd_score_2015, y = HLE)) +
+  geom_point(aes(colour = score), show.legend = FALSE) +
+  geom_smooth(method = lm, fullrange = TRUE, colour = 'grey') +
+  scale_colour_scico(palette = 'buda', direction = -1) +
+  scale_y_continuous(breaks = c(55, 60, 65, 70), limits = c(50, 75)) +
+  xlab("English Index of Multiple Deprivation") +
+  ylab("Healthy Life Expectancy") +
+  transition_manual(id) +
+  ease_aes('linear') +
+  theme_ver()
+
+anim_save(models, filename = "models_larger.gif", fps = 3)
 
